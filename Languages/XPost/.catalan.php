524394f6db164dbfb7f6090cc04a611ee03baa84
smf-prdx <administrador@cientoseis.es>
2025-07-31 13:07:58 +0200

    Updated repository structure to match SMF mod standards.

---
+++ Languages/XPost/.catalan.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @package prdx:xpost
+ */
+
+$txt['xpost_bbc'] = 'Insereix una publicació d\'X.com';
+$txt['xpost_theme'] = 'Tema d\'X.com';
+$txt['xpost_theme_light'] = 'Clar';
+$txt['xpost_theme_dark'] = 'Fosc';
+$txt['xpost_theme_auto'] = 'Automàtic';
+$txt['xpost_link_error'] = 'Només s\'accepten enllaços a publicacions individuals d\'X.com.';
+$txt['xpost_cant_load_tweet'] = 'No es pot carregar el post. Si us plau, verifica l\'enllaç o intenta-ho més tard.';
+$txt['xpost_not_authorized'] = 'La publicació és privada o no tens permís per veure-la.';
---
+++ Languages/XPost/.spanish_latin.php
@@ -0,0 +1,1 @@
+.spanish_es.php
